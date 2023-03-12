4216cb0d7b1d38d9727e10f250c533686fda8590
nikita <nbarabanschikov@mts.ru>
2023-03-12 21:17:37 +0300

    add configuration

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+resource "docker_image" "nginx"{
+  name = "nginx:1.23.3" # найти и скачать образ
+  keep_locally = false # образ будет удален из локально xранилища при destroy operation
+
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.1.23.3
+  name = "web_server"
+  ports {
+    internal = 80
+    
+  }
+
+
+}
