9f53ca2976086f7272236875e83c9caead5a2d48
nikita <nbarabanschikov@mts.ru>
2023-03-15 11:44:49 +0300

    fix app

--- docker-compose/app1/app1.py
+++ docker-compose/app1/app1.py
@@ -3,6 +3,6 @@ import json
 app1 = Flask(__name__)
 @app1.route('/')
 def hello_world():
-return 'Salam alikom, this is App1 :) '
+return 'Salam alikom, this is App2 :) '
 if __name__ == '__main__':
 app1.run(debug=True, host='0.0.0.0')
